88a916ce3541a061d7bdf14c8ffdf68550aedce1
GB-Tocix <41319615+GB-Tocix@users.noreply.github.com>
2019-05-27 18:41:58 +0800

    使用单一app.py起步
    使用单一app.py起步

--- app.py
+++ app.py
@@ -1,21 +1,82 @@
-#!usr/bin/env python
 import os
-from app import create_app, db
-from app.models import User, Role
-from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
-
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
-migrate = Migrate(app, db)
-
-
-def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+from flask import Flask, render_template, redirect, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
-manager.add_command("shell", Shell(make_context=make_shell_context()))
-manager.add_command('db', MigrateCommand)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-if __name__ == '__main__':
-    manager.run()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('.index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
