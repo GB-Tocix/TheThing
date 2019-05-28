f0535927b01d9609cdacb26b19c722fe2ae2f21c
GB-Tocix <41319615+GB-Tocix@users.noreply.github.com>
2019-05-28 09:42:20 +0800

    完成登录
    完成登录

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
+#!usr/bin/python
+
 import os
-from flask import Flask, render_template, redirect, session, url_for, flash
+from flask import Flask, render_template, redirect, session, url_for, flash, request
 from flask_bootstrap import Bootstrap
+from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, Form
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
+from wtforms import StringField, SubmitField, BooleanField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
+app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 class NameForm(FlaskForm):
@@ -38,9 +46,10 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
@@ -60,6 +69,28 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+
+def db_init():
+    db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/secret')
+@login_required
+def secret():
+    return 'Only authenticated users are allowed!'
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -74,9 +105,36 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('.index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form,
+                           name=session.get('name'), current_time=datetime.utcnow(),
+                           known=session.get('known', False))
+
+
+@app.route('/auth/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form, current_time=datetime.utcnow())
+
+
+@app.route('/auth/login')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
